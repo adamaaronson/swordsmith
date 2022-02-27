dbfb1a49aff27b4e7d49a0c313b06c02c80affc7
jjoshi8 <jjoshi8@illinois.edu>
2022-02-26 21:49:19 -0600

    Get main file working with CMake

---
+++ cpp/main.cpp
@@ -0,0 +1,15 @@
+/**
+ * @file main.cpp
+ * Jack Joshi, Adam Aaronson, JT Kirages, Mark Bauer
+ * February 2022
+ */
+
+#include <iostream>
+
+int main() {
+
+    std::cout << "It works!" << std::endl;
+
+    return 0;
+
+}
