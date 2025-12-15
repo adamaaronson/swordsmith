dbabc42236690625b2d5663d3d455babc0571c26
Adam Aaronson <adamaaronson@gmail.com>
2025-12-14 23:39:50 -0500

    Uncomment tests

--- tests/test_swordsmith.py
+++ tests/test_swordsmith.py
@@ -31,34 +31,34 @@ def grid_path(request):
     return request.param
 
 
-# def test_fill(filler, grid_path):
-#     grid = sw.read_grid(grid_path)
-#     crossword = sw.AmericanCrossword.from_grid(grid)
-#     wordlist = sw.read_wordlist(WORDLIST)
+def test_fill(filler, grid_path):
+    grid = sw.read_grid(grid_path)
+    crossword = sw.AmericanCrossword.from_grid(grid)
+    wordlist = sw.read_wordlist(WORDLIST)
 
-#     filler.fill(crossword, wordlist, animate=False)
+    filler.fill(crossword, wordlist, animate=False)
 
-#     assert crossword.is_validly_filled(wordlist)
+    assert crossword.is_validly_filled(wordlist)
 
 
-# @pytest.mark.parametrize(
-#     'constrained_slot',
-#     [
-#         ((0, 0), (0, 1), (0, 2), (0, 3), (0, 4)),
-#         ((4, 0), (4, 1), (4, 2), (4, 3), (4, 4)),
-#     ],
-# )
-# @pytest.mark.parametrize('regex', [r'^[^AEIOUY]*$', r'^[AEIOUY]*$'])
-# def test_regex_constraints(constrained_slot, regex, filler):
-#     grid = sw.read_grid(GRID_5x)
-#     crossword = sw.AmericanCrossword.from_grid(grid)
-#     crossword.add_regex_constraint(constrained_slot, regex)
-#     wordlist = sw.read_wordlist(WORDLIST)
+@pytest.mark.parametrize(
+    'constrained_slot',
+    [
+        ((0, 0), (0, 1), (0, 2), (0, 3), (0, 4)),
+        ((4, 0), (4, 1), (4, 2), (4, 3), (4, 4)),
+    ],
+)
+@pytest.mark.parametrize('regex', [r'^[^AEIOUY]*$', r'^[AEIOUY]*$'])
+def test_regex_constraints(constrained_slot, regex, filler):
+    grid = sw.read_grid(GRID_5x)
+    crossword = sw.AmericanCrossword.from_grid(grid)
+    crossword.add_regex_constraint(constrained_slot, regex)
+    wordlist = sw.read_wordlist(WORDLIST)
 
-#     filler.fill(crossword, wordlist, animate=False)
+    filler.fill(crossword, wordlist, animate=False)
 
-#     assert crossword.is_validly_filled(wordlist)
-#     assert re.search(regex, crossword.words[constrained_slot]) is not None
+    assert crossword.is_validly_filled(wordlist)
+    assert re.search(regex, crossword.words[constrained_slot]) is not None
 
 
 @pytest.mark.parametrize(
